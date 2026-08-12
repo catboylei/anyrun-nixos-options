1ac6c2dfd556d498b44e19481ae9e3e59019d24c
catboylei <243333633+catboylei@users.noreply.github.com>
2026-08-13 01:12:41 +0200

    remove flake parts

--- flake.nix
+++ flake.nix
@@ -1,46 +1,38 @@
 {
   description = "An anyrun plugin that lets you search NixOS options.";
 
-  inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
-  };
-
   outputs = inputs @ {
     nixpkgs,
-    flake-parts,
     ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
+  }: let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
 
-      perSystem = {
-        config,
-        pkgs,
-        ...
-      }: rec {
-        packages = rec {
-          anyrun-nixos-options = pkgs.callPackage ./nix {};
-          default = anyrun-nixos-options;
-        };
+    forEachSystem = f: nixpkgs.lib.genAttrs systems f;
+  in {
+    packages = forEachSystem (sys: let
+      pkgs = nixpkgs.legacyPackages.${sys};
+      anyrun-nixos-options = pkgs.callPackage ./package.nix {};
+    in {
+      inherit anyrun-nixos-options;
+      default = anyrun-nixos-options;
+    });
 
-        legacyPackages = packages;
-
-        devShells.default = with pkgs;
-          mkShell {
-            buildInputs = [
-              cargo
-              clippy
-              rustc
-              git
-              rustfmt
-              rust-analyzer
-            ];
-
-            inputsFrom = [packages.default];
-          };
+    devShells = forEachSystem (sys: let
+      pkgs = nixpkgs.legacyPackages.${sys};
+    in {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cargo
+          clippy
+          rustc
+          git
+          rustfmt
+          rust-analyzer
+        ];
       };
-    };
+    });
+  };
 }
--- package.nix
+++ package.nix
@@ -1,12 +1,12 @@
-{rustPlatform}:
+{ rustPlatform }:
 rustPlatform.buildRustPackage {
   pname = "anyrun-nixos-options";
   version = "0.1.0";
 
-  src = ../.;
+  src = ./.;
 
   cargoLock = {
-    lockFile = ../Cargo.lock;
+    lockFile = ./Cargo.lock;
     outputHashes."anyrun-interface-25.12.0" = "sha256-zcKI1OUg+Ukst0nasodrhKgBi61XT8vbvdK6/nuuApk=";
     outputHashes."anyrun-macros-26.6.1" = "sha256-+Fx+JfSboBk8KKVgmaMKDKvMe9c3WC+7RKYjnpvMVpg=";
   };
